21decfe8fc07d8491d09275571a85269655f4bc8
speschellk <kaychristensen@gmail.com>
2017-01-12 19:52:54 -0800

    Complete pluckFirstLine tests

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,20 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, cb) {
+  fs.readFile(filePath, function(err, data) {
+    if (err) {
+      return cb(err, data);
+    } else {
+      data = data.toString().split('\n')[0];
+      return cb(err, data);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url, callback) {
-  // TODO
+  console.log('url is ', url);
 };
 
 // Export these functions so we can test them and reuse them in later exercises
